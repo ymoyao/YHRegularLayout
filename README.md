23b7b7e08d7c48155705a1e75b1aa50344c086c1
游辉 <youhui@youhuideMacBook-Pro.local>
2019-07-21 17:03:15 +0800

    1.修复不左对称问题、2.修复单个label超出屏幕的情况、3.基础数据类型被错误转换的问题

--- README.md
+++ README.md
@@ -3,11 +3,13 @@
 * [**区别**](#区别)
 * [**事件响应链条**](#用法)
 * [**总结**](#总结)
+* [**修复记录**](#修复记录)
 
-###<a name="前言"></a>前言
+
+### <a name="前言"></a>前言
 在使用UICollectionViewFlowLayout 布局时发现一个问题：无法固定间距。所以我继承UICollectionViewFlowLayout写了一个类：YHRegularLayout。
 
-###<a name="区别"></a>区别
+### <a name="区别"></a>区别
 YHRegularLayout 是一种固定行 和 列间距的layout。
 无图无真相，看了图就明白他们之间的区别了
 
@@ -19,7 +21,7 @@ YHRegularLayout 是一种固定行 和 列间距的layout。
 
 很明显，UICollectionViewFlowLayout 在无法刚好充满屏幕时对该行的间距进行调整，使首尾刚好贴着两边。YHRegularLayout则反之，不调整间距，直接换行。
 
-###<a name="用法"></a>用法
+### <a name="用法"></a>用法
 正常使用即可，只是把设定的值，改 “最小” 为 “固定”，如下。
 
 ```
@@ -32,6 +34,12 @@ UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:CGRec
 
 ```
 
-
-###<a name="总结"></a>总结
+### <a name="总结"></a>总结
 [GitHub地址](https://github.com/developeryh/YHRegularLayout)
+
+### <a name="修复记录"></a>修复记录
+2019.7.21 (ps:感谢 @XiFengLang、@sdlbp 提的建议)
+1.修复不左对称问题
+2.修复单个label超出屏幕的情况
+3.基础数据类型被错误转换的问题
+
--- YHRegularLayout.m
+++ YHRegularLayout.m
@@ -10,47 +10,72 @@
 
 @implementation YHRegularLayout
 
-
 /**
  重写父类布局item的方法
-
+ 
  @param rect rect
  @return @[UICollectionViewLayoutAttributes]
  */
--(NSArray*)layoutAttributesForElementsInRect:(CGRect)rect{
+- (NSArray*)layoutAttributesForElementsInRect:(CGRect)rect{
     NSMutableArray* attributes = [[super layoutAttributesForElementsInRect:rect] mutableCopy];
-    for(int i = 1; i < [attributes count]; ++i) {
+    for(int i = 0; i < [attributes count]; ++i) {
         //当前attributes
         UICollectionViewLayoutAttributes *currentLayoutAttributes = attributes[i];
         //上一个attributes
-        UICollectionViewLayoutAttributes *prevLayoutAttributes = attributes[i - 1];
-        //固定间距
-        NSInteger maximumInteritemSpacing = self.minimumInteritemSpacing;
-        //固定行距
-        NSInteger maximumLineSpacing = self.minimumLineSpacing;
-        //获取上个item的最大X 和 Y
-        NSInteger originX = CGRectGetMaxX(prevLayoutAttributes.frame);
-        NSInteger originY = CGRectGetMaxY(prevLayoutAttributes.frame);
-   
-        
-        if(originX + maximumInteritemSpacing + currentLayoutAttributes.frame.size.width + self.sectionInset.right < self.collectionViewContentSize.width ) {                      //判断添加上该item 是否回超过屏幕
-            CGRect frame = currentLayoutAttributes.frame;
-            if (currentLayoutAttributes.indexPath.section == prevLayoutAttributes.indexPath.section) {//同一组才需要改x
-                frame.origin.x = originX + maximumInteritemSpacing;
-            }
-            currentLayoutAttributes.frame = frame;
-        }else if (currentLayoutAttributes.representedElementKind != nil){//头部视图不做处理
-            
+        UICollectionViewLayoutAttributes *prevLayoutAttributes;
+        if (i == 0) {
+            prevLayoutAttributes = nil;
+        } else {
+            prevLayoutAttributes = attributes[i - 1];
         }
-        else{                                                            //换行情况
-            CGRect frame = currentLayoutAttributes.frame;
-            if (currentLayoutAttributes.indexPath.section == prevLayoutAttributes.indexPath.section) {//同一组才需要改y
-                frame.origin.y = originY + maximumLineSpacing;
+        //固定间距
+        CGFloat maximumInteritemSpacing = self.minimumInteritemSpacing;
+        //固定行距
+        CGFloat maximumLineSpacing = self.minimumLineSpacing;
+        //获取上个item的最大X 和 Y
+        CGFloat originX = [self fetchOriginXWithCurrentLayoutAttributes:currentLayoutAttributes prevLayoutAttributes:prevLayoutAttributes];
+        CGFloat originY = [self fetchOriginYWithCurrentLayoutAttributes:currentLayoutAttributes prevLayoutAttributes:prevLayoutAttributes];
+        CGFloat currentAttWidth = currentLayoutAttributes.frame.size.width;
+        CGFloat sectionInsetLeft = self.sectionInset.left;
+        CGFloat sectionInsetRight = self.sectionInset.right;
+        CGFloat contentSizeWidth = self.collectionViewContentSize.width;
+        
+        if (currentLayoutAttributes.representedElementKind == nil) {                                                                    //头部视图不做处理
+            if (originX + maximumInteritemSpacing + currentAttWidth + sectionInsetRight < contentSizeWidth) {                           //判断添加上该item 是否回超过屏幕
+                CGRect frame = currentLayoutAttributes.frame;
+                if (currentLayoutAttributes.indexPath.section == prevLayoutAttributes.indexPath.section) {                              //同一组才需要改x
+                    frame.origin.x = originX + (prevLayoutAttributes?maximumInteritemSpacing:0);
+                }
+                currentLayoutAttributes.frame = frame;
+            } else {                                                                                                                     //换行情况
+                CGRect frame = currentLayoutAttributes.frame;
+                if (currentLayoutAttributes.indexPath.section == prevLayoutAttributes.indexPath.section) {                               //同一组才需要改y
+                    frame.origin.y = originY + (prevLayoutAttributes?maximumLineSpacing:0);
+                    frame.origin.x = sectionInsetLeft;
+                    if (currentAttWidth + sectionInsetLeft + sectionInsetRight > contentSizeWidth) {                                     //单个label 超出屏幕
+                        frame.size.width = contentSizeWidth - sectionInsetLeft - sectionInsetRight;
+                    }
+                }
+                currentLayoutAttributes.frame = frame;
             }
-            currentLayoutAttributes.frame = frame;
         }
     }
     return attributes;
+}
+
+
+- (CGFloat)fetchOriginXWithCurrentLayoutAttributes:(UICollectionViewLayoutAttributes *)currentLayoutAttributes prevLayoutAttributes:(UICollectionViewLayoutAttributes *)prevLayoutAttributes {
+    if (prevLayoutAttributes == nil) {
+        return self.sectionInset.left;
+    }
+    return CGRectGetMaxX(prevLayoutAttributes.frame);
+}
+
+- (CGFloat)fetchOriginYWithCurrentLayoutAttributes:(UICollectionViewLayoutAttributes *)currentLayoutAttributes prevLayoutAttributes:(UICollectionViewLayoutAttributes *)prevLayoutAttributes {
+    if (prevLayoutAttributes == nil) {
+        return CGRectGetMinY(currentLayoutAttributes.frame);
+    }
+    return CGRectGetMaxY(prevLayoutAttributes.frame);
 }
 
 @end
